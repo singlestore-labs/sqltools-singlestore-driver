57957d70df4002ccecac04b4bb931e06863f7ed5
Adalbert Makarovych <amakarovych-ua@memsql.com>
2023-02-22 21:20:55 +0200

    Updated config files
    Summary:
    Updated dependencies to the latest version.
    Deleted unused dependencies.
    Added new information in package.json.
    Other nit changes.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "<rootDir>/src",
   ],
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
